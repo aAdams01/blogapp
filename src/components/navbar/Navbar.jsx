0be85723c8232f4f2b022690f27b64c362d9bd49
Ariel Adams <99680748+aAdams01@users.noreply.github.com>
2024-07-09 21:29:30 -0600

    finish navbar, start outline for home page and footer WIP

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
               </svg>
           </button>
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky" style={{display: isOpen ? 'block' : 'none'}}>
+        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a href="#" className="block py-2 px-3 text-white bg-indigo-400 rounded md:bg-transparent md:text-indigo-400 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
