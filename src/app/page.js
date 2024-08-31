de9f970d0ba535f4f0c240c86e7d4ab511506069
Ariel Adams <99680748+aAdams01@users.noreply.github.com>
2024-08-31 08:36:17 -0600

    start hero component WIP

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>TODO: Header</div>
+      <Hero />
       <div>TODO: About</div>
       <div>TODO: Recent book reviews/blog posts</div>
     </main>
