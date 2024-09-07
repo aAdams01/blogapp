12efc3d1471122635e5cd465461f45b22cd00ece
Ariel Adams <99680748+aAdams01@users.noreply.github.com>
2024-09-06 20:59:28 -0600

    add accent colors WIP

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,12 +4,12 @@ export default function Hero() {
   return (
   <section>
     <div>
-      <h1 className="my-7 text-center text-3xl md:text-5xl">Dev & Reader</h1>
+      <h1 className="my-7 text-center text-3xl md:text-5xl">Web Developer</h1>
       <div className="grid grid-cols-1 md:grid-cols-4">
         {/* Column 1: Mini About Me */}
         <div className="diamond-container">
           <div className="diamond">
-            <h2>Ariel</h2>
+            <h2 className="md:text-2xl text-lg uppercase font-medium">Ariel</h2>
             <h3>Web Developer</h3>
             <p>Location</p>
             <p>Coding</p>
@@ -19,10 +19,10 @@ export default function Hero() {
           </div>
         </div>
         {/* Column 2: Site Intro + CTA */}
-        <div className="col-span-2">
-          <h2>Welcome, <br /> 
-              I'm Ariel. <br />
-              Mom, Reader, Developer
+        <div className="md:col-span-2">
+          <h2 className="font-medium md:text-2xl text-lg">Welcome, <br /> 
+              I'm <span className="text-lavendar">Ariel</span>. <br />
+              Mom, <span className="text-lavendar">Reader</span>, Developer
           </h2>
           <h3>I'm web developer turned stay-at-home mom who loves to code and read in my free time. Whether it's during nap time or after tucking the kids into bed, you'll find me working on random software projects or diving into a new book. Sometimes both.</h3>
           <a href="">Read the Blog</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'mint': '#08B49A',
+        'lavendar': '#7F8BF5'
+      },
     },
   },
   plugins: [],
